a44d96762e9c915b8be6bc310c89f2ba0c77ce71
lucas yonekura <lucasyonekura05@gmail.com>
2024-10-07 21:57:53 -0300

    Add files via upload

---
+++ ComboBoxTest.java
@@ -0,0 +1,13 @@
+
+import javax.swing.*;//importando biblioteca
+
+public class ComboBoxTest//declarando classe
+{//abertura de classe
+   public static void main(String[] args)//declarando método estático com retorno nulo
+   { 
+      ComboBoxFrame comboBoxFrame = new ComboBoxFrame(); 
+      comboBoxFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      comboBoxFrame.setSize(350, 150); 
+      comboBoxFrame.setVisible(true); 
+   } 
+} 
